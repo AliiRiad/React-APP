6fb06010957bb687bf7c78c7279ab1527f07af66
Riad Ali <alii.riad1234@gmail.com>
2024-12-03 15:48:43 +0100

    Home.js modified

--- src/Home.js
+++ src/Home.js
@@ -1,52 +1,67 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-    const data = useSelector(state=>state)
-    const dispatch =useDispatch()
-    const [ville,setVille]=useState("")
-  return (
+  const data = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const [ville, setVille] = useState('');
+  const villes = data.map(elem => elem.ville).filter((ville, index, self) => self.indexOf(ville) === index);
+    console.log(villes)
+    return (
     <>
-        <h1>Liste des contacts</h1>
-        <button><Link to="/AddContact" >ADD CONTACT +</Link></button>
-        
-        <h3>Filter</h3>
-        <select onChange={(e)=>setVille(e.target.value)} >
-            <option value="" >Votre Ville</option>
-            <option value="agadir" >Agadir</option>
-            <option value="tiznit" >Tiznit</option>
-            <option value="casablanca" >Casablanca</option>
-        </select>
-        <button onClick={()=>dispatch({type:'filter',ville:ville})} >Filtrer</button>
-        <button onClick={()=>dispatch({type:"initiale"})} >Initialiser</button>
+      <h1>Liste des contacts</h1>
+      <button>
+        <Link to="/AddContact">ADD CONTACT +</Link>
+      </button>
 
-        <table>
-            <thead>
-                <tr>
-                    <th>#ID</th>
-                    <th>NOM</th>
-                    <th>ADRESS</th>
-                    <th>VILLE</th>
-                    <th>HANDLE</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((elem,index)=>
-                    <tr key={index}>
-                        <td>{index}</td>
-                        <td>{elem.nom}</td>
-                        <td>{elem.adress}</td>
-                        <td>{elem.ville}</td>
-                        <td>
-                            <button onClick={()=>dispatch({type:"remove",index:index})} >Supprimer</button>
-                            <button><Link to={`/Details/${index}`} >Details</Link></button>
-                            <button><Link to={`/Edit/${index}`} >Edit</Link></button>
-                        </td>
-                    </tr>
-                )}
-            </tbody>
-        </table>
+      <h3>Filter</h3>
+      <select
+        value={ville}
+        onChange={(e) => setVille(e.target.value)}
+      >
+        <option value="">Votre Ville</option>
+        <option></option>
+      </select>
+      <button onClick={() => dispatch({ type: 'filter', ville: ville })}>
+        Filtrer
+      </button>
+      <button onClick={() => dispatch({ type: 'initiale' })}>Initialiser</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>#ID</th>
+            <th>NOM</th>
+            <th>ADRESS</th>
+            <th>VILLE</th>
+            <th>HANDLE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((elem, index) => (
+            <tr key={index}>
+              <td>{index}</td>
+              <td>{elem.nom}</td>
+              <td>{elem.adress}</td>
+              <td>{elem.ville}</td>
+              <td>
+                <button
+                  onClick={() => dispatch({ type: 'remove', index: index })}
+                >
+                  Supprimer
+                </button>
+                <button>
+                  <Link to={`/Details/${index}`}>Details</Link>
+                </button>
+                <button>
+                  <Link to={`/Edit/${index}`}>Edit</Link>
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
-  )
+  );
 }
